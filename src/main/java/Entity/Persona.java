1cdcf40e54e937dd491bfc24a4b76b23b79f43ae
emiliofsilvay <emilio@adaptativamente.cl>
2020-12-28 22:24:52 -0300

    carga inicial

---
+++ src/main/java/Entity/Persona.java
@@ -0,0 +1,43 @@
+package Entity;
+
+public class Persona {
+    
+    private int id_persona;
+    private String nombre;
+    private String apellido;
+    private Rol rol;
+
+    public int getId_persona() {
+        return id_persona;
+    }
+
+    public void setId_persona(int id_persona) {
+        this.id_persona = id_persona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+    
+    
+}
